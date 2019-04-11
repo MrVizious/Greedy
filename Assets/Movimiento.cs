26e7311a7578f4947072e44c802773cf8d8e958a
Sebastián Morcillo <sebas.morcillo@gmail.com>
2019-04-12 00:12:44 +0200

    Levelv1 + MovimientoBasico + RespetarLimites

---
+++ Assets/Movimiento.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Movimiento : MonoBehaviour
+{
+    [SerializeField] private float runSpeed;
+    protected Vector2 direccion;
+    
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        Move();
+    }
+
+    public void Move() {
+        transform.Translate(direccion * runSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : Movimiento
+{
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+
+        base.Update();
+    }
+
+    private void GetInput() {
+        direccion = Vector2.zero;
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                direccion += Vector2.left;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                direccion += Vector2.right;
+            }
+        }
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.W))
+        {
+            if (Input.GetKey(KeyCode.S))
+            {
+                direccion += Vector2.down;
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                direccion += Vector2.up;
+            }
+        }
+    }
+}
