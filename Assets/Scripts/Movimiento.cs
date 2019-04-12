f28515ed241458674d0709252cd1879093caaddb
Sebastián Morcillo <sebas.morcillo@gmail.com>
2019-04-12 11:33:44 +0200

    Cambios organizativos

--- Assets/Scripts/Movimiento.cs
+++ Assets/Scripts/Movimiento.cs
@@ -6,7 +6,13 @@ public abstract class Movimiento : MonoBehaviour
 {
     [SerializeField] private float runSpeed;
     protected Vector2 direccion;
-    
+
+    private void Start()
+    {
+        if (runSpeed == 0f) {
+            runSpeed = 5f;
+        }
+    }
 
     // Update is called once per frame
     protected virtual void Update()
