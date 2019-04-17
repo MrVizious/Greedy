0b2440207d5761908d39cb30c7edb86342ab5dfb
Javier Riera Chirivella <mrvizious@gmail.com>
2019-04-18 00:44:24 +0200

    Añadidos raycasts para movimiento, Marcador creado

--- Assets/Scripts/Movimiento.cs
+++ Assets/Scripts/Movimiento.cs
@@ -2,62 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movimiento : MonoBehaviour
-{
-    [SerializeField] private float runSpeed;
-    protected Vector3 posicionObjetivo;
-    protected Vector3 direccion;
-    protected Vector3 direccionGuardian;
+public class Movimiento : MonoBehaviour {
+	[SerializeField]
+	private float runSpeed;
+	[SerializeField]
+	[RangeAttribute(0f, 1f)]
+	private float anguloRaycast;
+	protected Vector3 posicionObjetivo;
+	protected Vector3 direccion;
+	protected Vector3 direccionGuardian;
 
-    private int capasQueColisionan;
+	private int capasDeColision;
 
-    //protected RaycastHit2D hit;
+	//protected RaycastHit2D hit;
 
-    private void Start()
-    {
-        capasQueColisionan = 1 << LayerMask.NameToLayer("obstaculo");
-        posicionObjetivo = transform.position;
+	private void Start() {
+		capasDeColision = 1 << LayerMask.NameToLayer("obstaculo");
+		posicionObjetivo = transform.position;
 
-        if (runSpeed == 0f) {
-            runSpeed = 5f;
-        }
-    }
+		if (runSpeed == null || runSpeed == 0f) runSpeed = 5f;
+		if (anguloRaycast == null || anguloRaycast == 0) anguloRaycast = 0.35f;
+	}
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-         Move();
-    }
+	// Update is called once per frame
+	void FixedUpdate() {
+		Move();
+	}
 
-    public void Move() {
-         transform.position = Vector3.MoveTowards(transform.position, posicionObjetivo, Time.deltaTime * runSpeed);
-              
-    }
+	public void Move() {
+		transform.position = Vector3.MoveTowards(transform.position, posicionObjetivo, Time.deltaTime * runSpeed);
 
-    public void Move(Vector3 direccionG) {
-        transform.position = Vector3.MoveTowards(transform.position, direccionG, Time.deltaTime * runSpeed);
-    }
+	}
 
-    bool PuedeAvanzar(Vector3 direccion)
-    {
-        Debug.Log("Comprobando si puede avanzar");
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, direccion, 0.6f, capasQueColisionan);
-        Debug.DrawRay(transform.position, direccion, Color.blue, 0.6f);
-        if (hit.collider != null) {
-            Debug.Log("No puede avanzar, ha chocado con: " + hit.collider.tag);
-            if (hit.collider.tag == "obstaculo") {
-                return false;
-            }
-        }
-        Debug.Log("Puede avanzar");
-        return true;
+	public void Move(Vector3 direccionG) {
+		transform.position = Vector3.MoveTowards(transform.position, direccionG, Time.deltaTime * runSpeed);
+	}
 
-    }
+	/// <summary>
+	/// Este método se encarga de comprobar con 3 raycasts que se pueda avanzar en una determinada dirección
+	/// </summary>
+	/// <param name="direccion">Vector2.up, down, left o right</param>
+	/// <returns>True si no hay colisión con obstáculo, false si la hay y por tanto no se puede avanzar</returns>
+	bool PuedeAvanzar(Vector3 direccion) {
+		Debug.Log("Comprobando si puede avanzar");
+		RaycastHit2D hit1 = Physics2D.Raycast(transform.position, direccion, 1.2f, capasDeColision);
+		Debug.DrawRay(transform.position, direccion, Color.blue, 1.2f);
+		RaycastHit2D hit2 = Physics2D.Raycast(transform.position, new Vector2(direccion.x == 0 ? -anguloRaycast : direccion.x, direccion.y == 0 ? -anguloRaycast : direccion.y), 1.2f, capasDeColision);
+		Debug.DrawRay(transform.position, new Vector2(direccion.x == 0 ? -anguloRaycast : direccion.x, direccion.y == 0 ? -anguloRaycast : direccion.y), Color.red, 1.2f);
+		RaycastHit2D hit3 = Physics2D.Raycast(transform.position, new Vector2(direccion.x == 0 ? anguloRaycast : direccion.x, direccion.y == 0 ? anguloRaycast : direccion.y), 1.2f, capasDeColision);
+		Debug.DrawRay(transform.position, new Vector2(direccion.x == 0 ? anguloRaycast : direccion.x, direccion.y == 0 ? anguloRaycast : direccion.y), Color.green, 1.2f);
 
-    public void SetRumbo(Vector2 direccion) {
-        if (PuedeAvanzar(direccion)) {
-            posicionObjetivo = new Vector2(Mathf.Round(transform.position.x + direccion.x),
-                Mathf.Round(transform.position.y + direccion.y));
-        }
-    }
+		if (hit1.collider != null && hit1.collider.tag.Equals("obstaculo")) return false;
+		if (hit2.collider != null && hit2.collider.tag.Equals("obstaculo")) return false;
+		if (hit3.collider != null && hit3.collider.tag.Equals("obstaculo")) return false;
+		Debug.Log("Puede avanzar");
+		return true;
+
+	}
+
+	/// <summary>
+	/// Comprueba que en la dirección en la que se quiere mover no haya ningún obstáculo, y si es así, cambia el rumbo a esa casilla
+	/// </summary>
+	/// <param name="direccion">Vector2.up, down, right o left</param>
+	public void SetRumbo(Vector2 direccion) {
+		if (PuedeAvanzar(direccion)) {
+			posicionObjetivo = new Vector2(Mathf.Round(transform.position.x + direccion.x),
+				Mathf.Round(transform.position.y + direccion.y));
+		}
+	}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,92 +1,76 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(Movimiento))]
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
 
-    int dañoAcumulado;
-    int caloriasAcumuladas;
-    int caloriasParaReducir = 100;
-    int reduccionPorCalorias = 10;
-    Fruta frutaQueCome;
-    bool comer;
-    protected bool arriba, abajo, derecha, izquierda;
-    private Movimiento movimiento;
-
-
-    void Start () {
-        //posicionObjetivo = transform.position;
-        movimiento = GetComponent<Movimiento>();
-        dañoAcumulado = 0;
-        caloriasAcumuladas = 0;
-        arriba = abajo = derecha = izquierda = false;
-    }
+	int dañoAcumulado;
+	int caloriasAcumuladas;
+	int caloriasParaReducir = 100;
+	int reduccionPorCalorias = 10;
+	Fruta frutaQueCome;
+	bool comer;
+	protected bool arriba, abajo, derecha, izquierda;
+	private Movimiento movimiento;
 
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (arriba)
-        {
-            movimiento.SetRumbo(Vector2.up);
-        }
-        else if (abajo)
-        {
-            movimiento.SetRumbo(Vector2.down);
-        }
-        else if (derecha)
-        {
-            movimiento.SetRumbo(Vector2.right);
-        }
-        else if (izquierda)
-        {
-            movimiento.SetRumbo(Vector2.left);
-        }
-        if (frutaQueCome != null) {
-            if (comer)
-            {
-                frutaQueCome.Desaparecer();
-            }
-        }
-
-        arriba = false;
-        abajo = false;
-        derecha = false;
-        izquierda = false;
-
-        comer = false;
-
-    }
+	void Start() {
+		//posicionObjetivo = transform.position;
+		movimiento = GetComponent<Movimiento>();
+		dañoAcumulado = 0;
+		caloriasAcumuladas = 0;
+		arriba = abajo = derecha = izquierda = false;
+	}
 
 
-    public void SetIzquierdaTrue() {
-        izquierda = true;
-    }
-    public void SetDerechaTrue()
-    {
-        derecha = true;
-    }
-    public void SetArribaTrue()
-    {
-        arriba = true;
-    }
-    public void SetAbajoTrue()
-    {
-        abajo = true;
-    }
+	// Update is called once per frame
+	void Update() {
+		if (arriba) {
+			movimiento.SetRumbo(Vector2.up);
+		} else if (abajo) {
+			movimiento.SetRumbo(Vector2.down);
+		} else if (derecha) {
+			movimiento.SetRumbo(Vector2.right);
+		} else if (izquierda) {
+			movimiento.SetRumbo(Vector2.left);
+		}
+		if (frutaQueCome != null) {
+			if (comer) {
+				frutaQueCome.Desaparecer();
+			}
+		}
 
-    public void SetComerTrue() {
-        comer = true;
-    }
+		arriba = false;
+		abajo = false;
+		derecha = false;
+		izquierda = false;
 
-    private void OnTriggerStay2D(Collider2D colisionador)
-    {
-        if (colisionador.tag == "fruta")
-        {
-            frutaQueCome = colisionador.gameObject.GetComponent<Fruta>();
-        }
-    }
+		comer = false;
+
+	}
+
+
+	public void SetIzquierdaTrue() {
+		izquierda = true;
+	}
+	public void SetDerechaTrue() {
+		derecha = true;
+	}
+	public void SetArribaTrue() {
+		arriba = true;
+	}
+	public void SetAbajoTrue() {
+		abajo = true;
+	}
+
+	public void SetComerTrue() {
+		comer = true;
+	}
+
+	private void OnTriggerStay2D(Collider2D colisionador) {
+		if (colisionador.tag == "fruta") {
+			frutaQueCome = colisionador.gameObject.GetComponent<Fruta>();
+		}
+	}
 }
