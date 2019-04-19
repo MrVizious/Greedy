bed78d50d77cf579c05f0c921564ea5fcf3e4ee7
Sebastián Morcillo <sebas.morcillo@gmail.com>
2019-04-19 17:52:04 +0200

    Primera version del movimiento del guardian
    llega un momento que se mueve todo el rato igual xd

--- Assets/Scripts/Guardian.cs
+++ Assets/Scripts/Guardian.cs
@@ -2,6 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Guardian {
+[RequireComponent(typeof(Movimiento))]
+public class Guardian : MonoBehaviour {
+    Vector2 direccionElegida;
+    Movimiento movimiento;
+
+    void Start() {
+        movimiento = GetComponent<Movimiento>();
+
+        CalculaDirRandom();
+        movimiento.SetRumbo(direccionElegida);
+    }
+
+
+    void Update() {
+        if (movimiento.PuedeAvanzar(direccionElegida))
+        {
+            movimiento.SetRumbo(direccionElegida);
+        }
+        else {
+            CalculaDirRandom();
+        }
+    }
+
+    void CalculaDirRandom() {
+        int elegido = Random.Range(0, 3);
+        Debug.Log(elegido);
+        switch (elegido) {
+            case 0:
+                direccionElegida = Vector2.up;
+                break;
+            case 1:
+                direccionElegida = Vector2.down;
+                break;
+            case 2:
+                direccionElegida = Vector2.right;
+                break;
+            case 3:
+                direccionElegida = Vector2.left;
+                break;
+        }
+    }
 
 }
