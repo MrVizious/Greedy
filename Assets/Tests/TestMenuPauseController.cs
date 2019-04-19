cc2e9f9ce14648c231a0b8d7dbb672e6e49c0105
Germán <german98_xtv@hotmail.com>
2019-04-19 19:47:42 +0200

    Test PauseMenu acabado

--- Assets/Tests/TestMenuPauseController.cs
+++ Assets/Tests/TestMenuPauseController.cs
@@ -12,17 +12,20 @@ namespace Tests
         [Test]
         public void IsGamePausedTest()
         {
-            prefabPauseMenu = (GameObject)Resources.Load("Test/MenuPausa");
-            GameObject objetoInicializado = Object.Instantiate(prefabPauseMenu, new Vector2(0, 0), Quaternion.identity);
+            prefabPauseMenu = (GameObject)Resources.Load("Tests/PauseMenu");
+            GameObject objetoInicializado = (GameObject) Object.Instantiate(prefabPauseMenu, new Vector2(0, 0), Quaternion.identity);
             PauseMenuController objetoTest = objetoInicializado.GetComponent<PauseMenuController>();
+            Assert.IsFalse(objetoTest.IsGamePaused());
             objetoTest.PauseGame();
             Assert.IsTrue(objetoTest.IsGamePaused());
+            objetoTest.ResumeGame();
+            Assert.IsFalse(objetoTest.IsGamePaused());
         }
 
         [Test]
         public void PauseGameTest()
         {
-            prefabPauseMenu = (GameObject)Resources.Load("Test/MenuPausa");
+            prefabPauseMenu = (GameObject)Resources.Load("Tests/PauseMenu");
             GameObject objetoInicializado = Object.Instantiate(prefabPauseMenu, new Vector2(0, 0), Quaternion.identity);
             PauseMenuController objetoTest = objetoInicializado.GetComponent<PauseMenuController>();
             objetoTest.PauseGame();
@@ -39,7 +42,7 @@ namespace Tests
         [Test]
         public void ResumeGameTest()
         {
-            prefabPauseMenu = (GameObject)Resources.Load("Test/MenuPausa");
+            prefabPauseMenu = (GameObject)Resources.Load("Tests/PauseMenu");
             GameObject objetoInicializado = Object.Instantiate(prefabPauseMenu, new Vector2(0, 0), Quaternion.identity);
             PauseMenuController objetoTest = objetoInicializado.GetComponent<PauseMenuController>();
             objetoTest.ResumeGame();
@@ -51,7 +54,6 @@ namespace Tests
             Assert.AreEqual(1f, Time.timeScale);
             Assert.IsFalse(objetoTest.PauseMenu.active);
             Assert.IsFalse(objetoTest.isPaused);
-            objetoTest.PauseGame();
         }
 
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
