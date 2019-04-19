f79961274377a609a4ddc882a3b80a477870b3e5
Javier Riera Chirivella <mrvizious@gmail.com>
2019-04-19 19:16:52 +0200

    Pause Menu Controller cambiado para evitar referencias extra

--- Assets/Scripts/PauseMenuController.cs
+++ Assets/Scripts/PauseMenuController.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PauseMenuController : MonoBehaviour {
-	public bool isPaused = false;
+	[SerializeField]
+	private bool isPaused = false;
+	private GameObject PauseMenu;
 
-	public bool IsGamePaused() {
-		return this.isPaused;
+	private void Start() {
+		PauseMenu = GetComponentInChildren<Canvas>().gameObject;
 	}
 
-	[SerializeField]
-	public GameObject PauseMenu;
 	void Update() {
 		if (Input.GetKeyDown(KeyCode.Escape)) {
 			if (isPaused) {
@@ -20,11 +20,15 @@ public class PauseMenuController : MonoBehaviour {
 			}
 		}
 	}
+	public bool IsGamePaused() {
+		return this.isPaused;
+	}
+
 
 	public void PauseGame() {
 		Time.timeScale = 0f;
-        isPaused = true;
-        PauseMenu.SetActive(true);
+		isPaused = true;
+		PauseMenu.SetActive(true);
 	}
 	public void ResumeGame() {
 		Time.timeScale = 1f;
---
+++ Assets/Scripts/SpawnerObjetos.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerObjetos : MonoBehaviour {
+	public int minX, minY, maxX, maxY;
+
+	//[]
+}
