f12853382d265f889873ccc102e8615569342eab
Javier Riera Chirivella <mrvizious@gmail.com>
2019-04-24 17:30:36 +0200

    Estado Invulnerable Programado

--- Assets/Scripts/AccionesNormal.cs
+++ Assets/Scripts/AccionesNormal.cs
@@ -3,53 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class AccionesNormal : Acciones
-{
-    public override void AumentarCalorias(int calorias, PlayerController player)
-    {
-        player.caloriasAcumuladas += calorias;
-    }
+public class AccionesNormal : Acciones {
+	public override void AumentarCalorias(int calorias, PlayerController player) {
+		player.caloriasAcumuladas += calorias;
+	}
 
-    public override void Morir(PlayerController player)
-    {
-        //TODO: de momento se reinicia la escena
-         SceneManager.LoadScene("SampleScene");
-    }
+	public override void Morir(PlayerController player) {
+		//TODO: de momento se reinicia la escena
+		SceneManager.LoadScene("SampleScene");
+	}
 
-    public override void RecibirDaño(int daño, PlayerController player)
-    {
-        player.dañoAcumulado = +daño;
-        if (player.dañoAcumulado >= 100)
-        {
-            //PlayerStats.restarVida();
-            player.dañoAcumulado = 0;
-        }
-    }
+	public override void RecibirDaño(int daño, PlayerController player) {
+		player.dañoAcumulado = +daño;
+		if (player.dañoAcumulado >= 100) {
+			//PlayerStats.restarVida();
+			player.dañoAcumulado = 0;
+		}
+	}
 
-    public override void ReducirDaño(PlayerController player)
-    {
-        if (player.caloriasAcumuladas >= player.caloriasParaReducir)
-        {
-            player.dañoAcumulado -= player.reduccionPorCalorias;
-            if (player.dañoAcumulado < 0) player.dañoAcumulado = 0;
-            player.caloriasAcumuladas -= player.caloriasParaReducir;
-        }
-    }
+	public override void ReducirDaño(PlayerController player) {
+		if (player.caloriasAcumuladas >= player.caloriasParaReducir) {
+			player.dañoAcumulado -= player.reduccionPorCalorias;
+			if (player.dañoAcumulado < 0) player.dañoAcumulado = 0;
+			player.caloriasAcumuladas -= player.caloriasParaReducir;
+		}
+	}
 
-    public override void RestablecerACero(PlayerController player)
-    {
-         player.dañoAcumulado = 0;
-    }
+	public override void RestablecerACero(PlayerController player) {
+		player.dañoAcumulado = 0;
+	}
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public override void PrepararParaCambiarEstado() {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	}
+
+	// Start is called before the first frame update
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,16 +6,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public int dañoAcumulado;
-    public int caloriasAcumuladas;
-    public int caloriasParaReducir = 100;
-    public int reduccionPorCalorias = 10;
+	public int caloriasAcumuladas;
+	public int caloriasParaReducir = 100;
+	public int reduccionPorCalorias = 10;
 	Fruta frutaQueCome;
 	bool comer;
 	public bool arriba, abajo, derecha, izquierda;
 	private Movimiento movimiento;
-    public Acciones estado;
-    Acciones defensa;
-    int duracionDefensa;
+	public Acciones estado;
+	Acciones defensa;
+	int duracionDefensa;
 
 	public void Start() {
 		//posicionObjetivo = transform.position;
@@ -23,29 +23,34 @@ public class PlayerController : MonoBehaviour {
 		dañoAcumulado = 0;
 		caloriasAcumuladas = 0;
 		arriba = abajo = derecha = izquierda = false;
-        estado = new GameObject().AddComponent<AccionesNormal>();
+		estado = gameObject.AddComponent<AccionesInvulnerable>();
 	}
 
 
 	// Update is called once per frame
 	void Update() {
 		if (arriba) {
-            movimiento.direccion = Vector2.up;
+			movimiento.direccion = Vector2.up;
 			movimiento.SetRumbo(movimiento.direccion);
 		} else if (abajo) {
-            movimiento.direccion = Vector2.down;
-            movimiento.SetRumbo(movimiento.direccion);
+			movimiento.direccion = Vector2.down;
+			movimiento.SetRumbo(movimiento.direccion);
 		} else if (derecha) {
-            movimiento.direccion = Vector2.right;
-            movimiento.SetRumbo(movimiento.direccion);
+			movimiento.direccion = Vector2.right;
+			movimiento.SetRumbo(movimiento.direccion);
 		} else if (izquierda) {
-            movimiento.direccion = Vector2.left;
-            movimiento.SetRumbo(movimiento.direccion);
+			movimiento.direccion = Vector2.left;
+			movimiento.SetRumbo(movimiento.direccion);
 		}
 		if (frutaQueCome != null) {
 			if (comer) {
 				frutaQueCome.Desaparecer();
 			}
+		}
+		//TODO: Quitar, esto es solo para debug
+		if (Input.GetKeyDown(KeyCode.E)) {
+			if (estado.GetType() == typeof(AccionesInvulnerable)) CambiarAEstadoNormal();
+			else CambiarAEstadoInvulnerable();
 		}
 
 		arriba = false;
@@ -55,6 +60,60 @@ public class PlayerController : MonoBehaviour {
 
 		comer = false;
 
+	}
+
+
+
+	private void CambiarAEstadoNormal() {
+		estado.PrepararParaCambiarEstado();
+		Destroy(estado);
+		estado = gameObject.AddComponent<AccionesNormal>();
+	}
+
+	private void CambiarAEstadoInvulnerable() {
+		estado.PrepararParaCambiarEstado();
+		Destroy(estado);
+		estado = gameObject.AddComponent<AccionesInvulnerable>();
+	}
+
+	private void OnTriggerStay2D(Collider2D colisionador) {
+		if (colisionador.tag == "fruta") {
+			frutaQueCome = colisionador.gameObject.GetComponent<Fruta>();
+		} else
+		if (colisionador.tag == "defensa") {
+			defensa = colisionador.gameObject.GetComponent<Acciones>();
+			Destroy(colisionador.gameObject);
+		} else
+		if (colisionador.tag == "capsula") {
+			RestablecerACero();
+			Destroy(colisionador.gameObject);
+		} else
+		if (colisionador.tag == "corazon") {
+			//PlayerStats.SumarVida();
+			Destroy(colisionador.gameObject);
+		}
+	}
+
+
+
+	public void RecibirDaño(int daño) {
+		estado.RecibirDaño(daño, this);
+	}
+
+	public void AumentarCalorias(int calorias) {
+		estado.AumentarCalorias(calorias, this);
+	}
+
+	public void ReducirDaño() {
+		estado.ReducirDaño(this);
+	}
+
+	public void RestablecerACero() {
+		estado.RestablecerACero(this);
+	}
+
+	public void Morir() {
+		estado.Morir(this);
 	}
 
 
@@ -74,61 +133,4 @@ public class PlayerController : MonoBehaviour {
 	public void SetComerTrue() {
 		comer = true;
 	}
-
-    private void VolverAEstadoNormal()
-    {
-        estado = new GameObject().AddComponent<AccionesNormal>();
-    }
-	private void OnTriggerStay2D(Collider2D colisionador) {
-		if (colisionador.tag == "fruta") {
-			frutaQueCome = colisionador.gameObject.GetComponent<Fruta>();
-		} else
-        if(colisionador.tag == "defensa")
-        {
-            defensa = colisionador.gameObject.GetComponent<Acciones>();
-            Destroy(colisionador.gameObject);
-        } else
-        if (colisionador.tag == "capsula")
-        {
-            RestablecerACero();
-            Destroy(colisionador.gameObject);
-        } else
-        if (colisionador.tag == "corazon")
-        {
-            //PlayerStats.SumarVida();
-            Destroy(colisionador.gameObject);
-        }
-    }
-
-    private void UsarDefensa()
-    {
-        CancelInvoke("VolverAEstadoNormal");
-        estado = defensa;
-        Invoke("VolverAEstadoNormal", duracionDefensa);
-    }
-
-    public void RecibirDaño(int daño)
-    {
-        estado.RecibirDaño(daño, this);
-    }
-
-    public void AumentarCalorias(int calorias)
-    {
-        estado.AumentarCalorias(calorias, this);
-    }
-
-    public void ReducirDaño()
-    {
-        estado.ReducirDaño(this);
-    }
-
-    public void RestablecerACero()
-    {
-        estado.RestablecerACero(this);
-    }
-
-    public void Morir()
-    {
-        estado.Morir(this);
-    }
 }
