b913a8bb51cb29bc938820a9121dce731c7eee14
Sebastián Morcillo <sebas.morcillo@gmail.com>
2019-04-26 13:03:14 +0200

    nada importante

--- Assets/Scripts/Guardian.cs
+++ Assets/Scripts/Guardian.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 [RequireComponent(typeof(Movimiento))]
 [RequireComponent(typeof(PlayerController))]
-[RequireComponent(typeof(Animator))]
+//[RequireComponent(typeof(Animator))]
 
 
 
@@ -13,12 +13,12 @@ public class Guardian : MonoBehaviour {
     Movimiento movimiento;
     public Transform objetivo;
     PlayerController player;
-    Animator animator;
+    //Animator animator;
 
     void Start() {
         movimiento = GetComponent<Movimiento>();
         player = GetComponent<PlayerController>();
-        animator = GetComponent<Animator>();
+        //animator = GetComponent<Animator>();
 
 
         CalculaDirRandom();
@@ -28,7 +28,7 @@ public class Guardian : MonoBehaviour {
     //Si player esta dentro de su rango lo persigue,
     // si no, sigue su curso hasta que no pueda avanzar
     void Update() {
-        animator.SetFloat("Speed", movimiento.runSpeed);
+        //animator.SetFloat("Speed", movimiento.runSpeed);
         if (movimiento.PuedeAvanzar(direccionElegida))
         {
              movimiento.SetRumbo(direccionElegida);
@@ -45,31 +45,31 @@ public class Guardian : MonoBehaviour {
         switch (elegido) {
             case 0:
                 direccionElegida = Vector2.up;
-                animator.SetBool("Down", false);
+                /*animator.SetBool("Down", false);
                 animator.SetBool("Right", false);
                 animator.SetBool("Left", false);
-                animator.SetBool("Up", true);
+                animator.SetBool("Up", true);*/
                 break;
             case 1:
                 direccionElegida = Vector2.down;
-                animator.SetBool("Up", false);
+                /*animator.SetBool("Up", false);
                 animator.SetBool("Right", false);
                 animator.SetBool("Left", false);
-                animator.SetBool("Down", true);
+                animator.SetBool("Down", true);*/
                 break;
             case 2:
                 direccionElegida = Vector2.right;
-                animator.SetBool("Down", false);
+                /*animator.SetBool("Down", false);
                 animator.SetBool("Up", false);
                 animator.SetBool("Left", false);
-                animator.SetBool("Right", true);
+                animator.SetBool("Right", true);*/
                 break;
             case 3:
                 direccionElegida = Vector2.left;
-                animator.SetBool("Down", false);
+                /*animator.SetBool("Down", false);
                 animator.SetBool("Up", false);
                 animator.SetBool("Right", false);
-                animator.SetBool("Left", true);
+                animator.SetBool("Left", true);*/
                 break;
         }
     }
