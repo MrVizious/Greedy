9f228c2f1ad5405b435d2c20e98292b70bc5766c
Germán <german98_xtv@hotmail.com>
2019-04-28 11:53:56 +0200

    Un poco de interfaz

---
+++ Assets/Scripts/Interfaz.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interfaz : MonoBehaviour
+{
+    public GameObject Player;
+    string calorias, daño, vidas, defensa;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
