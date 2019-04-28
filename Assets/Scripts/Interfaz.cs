3a8a7499f92d26885fec21a4efa65c1c8eb33149
Germán <german98_xtv@hotmail.com>
2019-04-28 14:19:23 +0200

    Interfaz hecha, falta mostrar vidas

--- Assets/Scripts/Interfaz.cs
+++ Assets/Scripts/Interfaz.cs
@@ -1,20 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Interfaz : MonoBehaviour
 {
-    public GameObject Player;
-    string calorias, daño, vidas, defensa;
+    public GameObject player;
+    public PlayerController statsPlayer;
+    public GameObject gameController;
+    public string calorias, danyo, vidas, defensa;
+    public Text textoCalorias, textoDanyo, textoVidas, textoDefensa;
     // Start is called before the first frame update
     void Start()
     {
-        Player = GetComponent<GameObject>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        statsPlayer = player.GetComponent<PlayerController>();
+        gameController = GameObject.Find("GameController");
+        textoCalorias = this.transform.Find("Calorias").GetComponent<Text>();
+        textoDanyo = this.transform.Find("Danyo").GetComponent<Text>();
+        textoVidas = this.transform.Find("Vidas").GetComponent<Text>();
+        textoDefensa = this.transform.Find("Defensa").GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //TODO: mostrar las vidas
+        textoCalorias.text = "Calorías: " + statsPlayer.caloriasAcumuladas;
+        textoDanyo.text = "Daño: " + statsPlayer.dañoAcumulado;
+        if(player.GetComponent<AccionesNormal>() == null)
+        {
+            textoDefensa.text = "Defensa: Invulnerable";
+        } else { textoDefensa.text = "Defensa: ninguna"; }
+        //textoVidas = "Vidas" + gameController.GetComponent<GameController>().vidas;
     }
 }
