eda6e78cfd832921246b0d609d329d0e8aff4fd0
Javier Riera Chirivella <mrvizious@gmail.com>
2019-04-29 19:23:39 +0200

    Comer ya suma la puntuación

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,15 +9,16 @@ public class PlayerController : MonoBehaviour {
 	public int caloriasAcumuladas;
 	public int caloriasParaReducir = 100;
 	public int reduccionPorCalorias = 10;
-	Fruta frutaQueCome;
+	private Fruta frutaQueCome;
 	//bool comer;
 	public bool arriba, abajo, derecha, izquierda;
 	private Movimiento movimiento;
 	public Acciones estado;
 	Acciones defensa;
 	int duracionDefensa;
-    AudioSource audioPlayer;
-    public AudioClip mover, comer, perderVida, ganarVida;
+	AudioSource audioPlayer;
+	public AudioClip mover, comer, perderVida, ganarVida;
+	public Marcador marcador;
 
 	public void Start() {
 		//posicionObjetivo = transform.position;
@@ -26,7 +27,7 @@ public class PlayerController : MonoBehaviour {
 		caloriasAcumuladas = 0;
 		arriba = abajo = derecha = izquierda = false;
 		estado = gameObject.AddComponent<AccionesNormal>();
-        audioPlayer = GetComponent<AudioSource>();
+		audioPlayer = GetComponent<AudioSource>();
 	}
 
 
@@ -35,20 +36,20 @@ public class PlayerController : MonoBehaviour {
 		if (arriba) {
 			movimiento.direccion = Vector2.up;
 			movimiento.SetRumbo(movimiento.direccion);
-            ActivarSonidoMover();
-        } else if (abajo) {
+			ActivarSonidoMover();
+		} else if (abajo) {
 			movimiento.direccion = Vector2.down;
 			movimiento.SetRumbo(movimiento.direccion);
-            ActivarSonidoMover();
-        } else if (derecha) {
+			ActivarSonidoMover();
+		} else if (derecha) {
 			movimiento.direccion = Vector2.right;
 			movimiento.SetRumbo(movimiento.direccion);
-            ActivarSonidoMover();
-        } else if (izquierda) {
+			ActivarSonidoMover();
+		} else if (izquierda) {
 			movimiento.direccion = Vector2.left;
 			movimiento.SetRumbo(movimiento.direccion);
-            ActivarSonidoMover();
-        }
+			ActivarSonidoMover();
+		}
 		/*if (frutaQueCome != null) {
             //ComerFruta();
 		}*/
@@ -67,29 +68,25 @@ public class PlayerController : MonoBehaviour {
 
 	}
 
-    void ActivarSonidoMover()
-    {
-        audioPlayer.clip = mover;
-        audioPlayer.Play();
-    }
+	void ActivarSonidoMover() {
+		audioPlayer.clip = mover;
+		audioPlayer.Play();
+	}
 
-    void ActivarSonidoComer()
-    {
-        audioPlayer.clip = comer;
-        audioPlayer.Play();
-    }
+	void ActivarSonidoComer() {
+		audioPlayer.clip = comer;
+		audioPlayer.Play();
+	}
 
-    void ActivarSonidoPerderVida()
-    {
-        audioPlayer.clip = perderVida;
-        audioPlayer.Play();
-    }
-    void ActivarSonidoGanarVida()
-    {
-        audioPlayer.clip = ganarVida;
-        audioPlayer.Play();
-    }
-    /*private void ComerFruta()
+	void ActivarSonidoPerderVida() {
+		audioPlayer.clip = perderVida;
+		audioPlayer.Play();
+	}
+	void ActivarSonidoGanarVida() {
+		audioPlayer.clip = ganarVida;
+		audioPlayer.Play();
+	}
+	/*private void ComerFruta()
     {
         if (comer)
         {
@@ -97,54 +94,50 @@ public class PlayerController : MonoBehaviour {
         }
     }*/
 
-    private void CambiarAEstadoNormal() {
+	private void CambiarAEstadoNormal() {
 		estado.PrepararParaCambiarEstado();
 		Destroy(estado);
 		estado = gameObject.AddComponent<AccionesNormal>();
 	}
 
 	public void CambiarAEstadoInvulnerable() {
-        CancelInvoke("CambiarAEstadoNormal");
-        estado.PrepararParaCambiarEstado();
+		CancelInvoke("CambiarAEstadoNormal");
+		estado.PrepararParaCambiarEstado();
 		//Destroy(estado);
-        Destroy(estado); //Necesario para el test, Destroy() no deja.
-        estado = gameObject.AddComponent<AccionesInvulnerable>();
-        Invoke("CambiarAEstadoNormal", 7);
+		Destroy(estado); //Necesario para el test, Destroy() no deja.
+		estado = gameObject.AddComponent<AccionesInvulnerable>();
+		Invoke("CambiarAEstadoNormal", 7);
 	}
 
 	private void OnTriggerStay2D(Collider2D colisionador) {
 		if (colisionador.tag == "fruta" && Input.GetKeyDown(KeyCode.Space)) {
-            colisionador.gameObject.GetComponent<Fruta>().Desaparecer();
-            ActivarSonidoComer();
-            //frutaQueCome = colisionador.gameObject.GetComponent<Fruta>();
-        }
-    }
+			colisionador.gameObject.GetComponent<Fruta>().Desaparecer();
+			ActivarSonidoComer();
 
-    private void OnTriggerEnter2D(Collider2D colisionador)
-    {
-        if (colisionador.tag == "defensa")
-        {
-            ActivarSonidoGanarVida();
-            Destroy(colisionador.gameObject);
-        }
-        else
-        if (colisionador.tag == "capsula")
-        {
-            ActivarSonidoGanarVida();
-            RestablecerACero();
-            Destroy(colisionador.gameObject);
-        }
-        else
-        if (colisionador.tag == "corazon")
-        {
-            //PlayerStats.SumarVida();
-            ActivarSonidoGanarVida();
-            Destroy(colisionador.gameObject);
-        }
-    }
+			marcador.sumarPuntuacion(100);
+			//frutaQueCome = colisionador.gameObject.GetComponent<Fruta>();
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D colisionador) {
+		if (colisionador.tag == "defensa") {
+			ActivarSonidoGanarVida();
+			Destroy(colisionador.gameObject);
+		} else
+		if (colisionador.tag == "capsula") {
+			ActivarSonidoGanarVida();
+			RestablecerACero();
+			Destroy(colisionador.gameObject);
+		} else
+		if (colisionador.tag == "corazon") {
+			//PlayerStats.SumarVida();
+			ActivarSonidoGanarVida();
+			Destroy(colisionador.gameObject);
+		}
+	}
 
 
-    public void RecibirDaño(int daño) {
+	public void RecibirDaño(int daño) {
 		estado.RecibirDaño(daño, this);
 	}
 
