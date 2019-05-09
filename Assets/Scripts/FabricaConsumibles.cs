05c44d269885319d326ae0c11fedf93b05ebf047
Sebastián Morcillo <sebas.morcillo@gmail.com>
2019-05-09 20:22:29 +0200

    Fabrica hecha

--- Assets/Scripts/FabricaConsumibles.cs
+++ Assets/Scripts/FabricaConsumibles.cs
@@ -2,17 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(IConsumible))]
 public class FabricaConsumibles : MonoBehaviour
 {
+    public IConsumible prefabFresa;
+    public IConsumible prefabUva;
+    public IConsumible prefabPimiento;
+    public IConsumible prefabZanahoria;
     // Start is called before the first frame update
     void Start()
     {
-        
+        prefabFresa = GetComponent<Fresa>();
+        prefabFresa = GetComponent<Uva>();
+        prefabFresa = GetComponent<Pimiento>();
+        prefabFresa = GetComponent<Zanahoria>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+
+    public IConsumible GetConsumible(string nombreConsumible) {
+        switch (nombreConsumible) {
+            case "Fresa":
+                return prefabFresa;
+            case "Uva":
+                return prefabUva;
+            case "Pimiento":
+                return prefabPimiento;
+            case "Zanahoria":
+                return prefabZanahoria;
+            default:
+                return null;
+
+        }
     }
 }
