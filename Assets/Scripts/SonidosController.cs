388a52bcc3dbeb192d6b49996a4b117b478384f5
Germán <german98_xtv@hotmail.com>
2019-05-18 21:44:43 +0200

    Actualizados algunos prefab

--- Assets/Scripts/SonidosController.cs
+++ Assets/Scripts/SonidosController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class SonidosController : MonoBehaviour
 {
-    // Start is called before the first frame update
     [SerializeField]
     AudioClip mover, comer, perderVida, ganarVida, limite;
     AudioSource audioSonidos;
