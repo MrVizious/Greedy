73e3470b37d501d77fd4f68138e2430a5523dbb7
Javier Riera Chirivella <mrvizious@gmail.com>
2019-05-21 13:49:19 +0200

    Sonido final de nivel arreglado
    Al menos en el nivel 1

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -5,67 +5,62 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 //[RequireComponent(typeof(GameManager))]
-public class LevelController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    [SerializeField]
-    int numeroFrutas;
-    [SerializeField]
-    int numeroTrampas;
-    [SerializeField]
-    int numeroGuardianes;
-    [SerializeField]
-    int numeroGuardianesRapidos;
+public class LevelController : MonoBehaviour {
+	// Start is called before the first frame update
+	[SerializeField]
+	int numeroFrutas;
+	[SerializeField]
+	int numeroTrampas;
+	[SerializeField]
+	int numeroGuardianes;
+	[SerializeField]
+	int numeroGuardianesRapidos;
 
-    public GameObject[] frutas;
-    public GameObject gameController;
-    public GameManager gameManager;
-    SpawnerElementos spawner;
-    string[] consumibles = { "fresa", "uva", "pimiento", "zanahoria" };
+	public GameObject[] frutas;
+	public GameObject gameController;
+	public GameManager gameManager;
+	SpawnerElementos spawner;
+	string[] consumibles = { "fresa", "uva", "pimiento", "zanahoria" };
 
-    private MusicaController controladorMusica;
-    PlayerController player;
+	private MusicaController controladorMusica;
+	PlayerController player;
 
-    public void Start()
-    {
-        gameManager = GameManager.getGameManager();
+	public void Start() {
+		gameManager = GameManager.getGameManager();
 
-        controladorMusica = GameObject.Find("AudioNivel").GetComponent<MusicaController>();
-        player = GameObject.Find("Player").GetComponent<PlayerController>();
+		controladorMusica = GameObject.Find("AudioNivel").GetComponent<MusicaController>();
+		player = GameObject.Find("Player").GetComponent<PlayerController>();
 
-        spawner = GameObject.Find("SpawnerElementos").GetComponent<SpawnerElementos>();
-        //Hay que ver que no se generen cerca de player
-        spawner.SpawnearElementos(numeroGuardianes, "guardian");
-        spawner.SpawnearElementos(numeroGuardianesRapidos, "guardianRapido");
-        //spawner.GenerarGuardianes(numeroGuardianes, prefabGuardian);
-        //spawner.GenerarGuardianes(numeroGuardianesRapidos, prefabGuardianRapido);
-        foreach (string cons in consumibles)
-        {
-            spawner.SpawnearElementos(numeroFrutas / 4, cons);
-        }
-        //spawner.SpawnearElementos(numeroFrutas, prefabFruta);
-        spawner.SpawnearElementos(numeroTrampas, "trampa");
-    }
+		spawner = GameObject.Find("SpawnerElementos").GetComponent<SpawnerElementos>();
+		//Hay que ver que no se generen cerca de player
+		spawner.SpawnearElementos(numeroGuardianes, "guardian");
+		spawner.SpawnearElementos(numeroGuardianesRapidos, "guardianRapido");
+		//spawner.GenerarGuardianes(numeroGuardianes, prefabGuardian);
+		//spawner.GenerarGuardianes(numeroGuardianesRapidos, prefabGuardianRapido);
+		foreach (string cons in consumibles) {
+			spawner.SpawnearElementos(numeroFrutas / 4, cons);
+		}
+		//spawner.SpawnearElementos(numeroFrutas, prefabFruta);
+		spawner.SpawnearElementos(numeroTrampas, "trampa");
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        frutas = GameObject.FindGameObjectsWithTag("fruta");
-        numeroFrutas = frutas.Length;
-        if(numeroFrutas==0)
-        {
-            StartCoroutine(SiguienteNivel());
-        }
-            
-    }
+	// Update is called once per frame
+	void Update() {
+		frutas = GameObject.FindGameObjectsWithTag("fruta");
+		numeroFrutas = frutas.Length;
+		if (numeroFrutas == 0) {
+			StartCoroutine(SiguienteNivel());
+		}
 
-    IEnumerator SiguienteNivel() {
-        controladorMusica.ActivarSonidoGanar();
+	}
 
-        player.GetComponent<Animator>().runtimeAnimatorController = player.GreedyGanar;
-        player.Morir();
-        yield return new WaitForSeconds(4f);
+	IEnumerator SiguienteNivel() {
+		if (controladorMusica.getAudioSonidos().clip != controladorMusica.getGanar()) controladorMusica.ActivarSonidoGanar();
 
-        gameManager.SiguienteEscena();
-    }
+		player.GetComponent<Animator>().runtimeAnimatorController = player.GreedyGanar;
+		player.Morir();
+		yield return new WaitForSeconds(4f);
+
+		gameManager.SiguienteEscena();
+	}
 }
--- Assets/Scripts/MusicaController.cs
+++ Assets/Scripts/MusicaController.cs
@@ -2,24 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MusicaController : MonoBehaviour
-{
-    [SerializeField]
-    AudioClip gameOver, ganar;
-    AudioSource audioSonidos;
-    void Start()
-    {
-        audioSonidos = GetComponent<AudioSource>();
-    }
-    public void ActivarSonidoGameOver()
-    {
-        audioSonidos.clip = gameOver;
-        audioSonidos.Play();
-    }
+public class MusicaController : MonoBehaviour {
+	[SerializeField]
+	AudioClip gameOver, ganar;
+	AudioSource audioSonidos;
+	void Start() {
+		audioSonidos = GetComponent<AudioSource>();
+	}
+	public void ActivarSonidoGameOver() {
+		audioSonidos.clip = gameOver;
+		audioSonidos.Play();
+	}
 
-    public void ActivarSonidoGanar() {
-        Debug.Log("Me cago en dios");
-        audioSonidos.clip = ganar;
-        audioSonidos.Play();
-    }
+	public void ActivarSonidoGanar() {
+		audioSonidos.clip = ganar;
+		audioSonidos.Play();
+	}
+
+	public AudioClip getGameOver() {
+		return gameOver;
+	}
+	public AudioClip getGanar() {
+		return ganar;
+	}
+	public AudioSource getAudioSonidos() {
+		return audioSonidos;
+	}
+
+
 }
