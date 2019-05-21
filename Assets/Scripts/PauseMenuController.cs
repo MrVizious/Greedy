4b6d0774e6d270c20ea39f58f00749b1e8c86c48
Germán <german98_xtv@hotmail.com>
2019-05-21 18:05:08 +0200

    Interfaz asea y ajustes en los niveles

--- Assets/Scripts/PauseMenuController.cs
+++ Assets/Scripts/PauseMenuController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenuController : MonoBehaviour {
 	[SerializeField]
@@ -37,7 +38,7 @@ public class PauseMenuController : MonoBehaviour {
 	}
 
 	public void QuitGame() {
-		Application.Quit();
-	}
+        SceneManager.LoadScene("MenuPrincipal");
+    }
 
 }
