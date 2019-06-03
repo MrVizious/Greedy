fa3f403116621337d7d2b2ffdc9bf78684352df6
Germán <german98_xtv@hotmail.com>
2019-06-03 16:10:30 +0200

    Último commit (esperemos)

--- Assets/Scripts/MenuPrincipal/AnimacionMenu.cs
+++ Assets/Scripts/MenuPrincipal/AnimacionMenu.cs
@@ -5,19 +5,12 @@ using UnityEngine;
 public class AnimacionMenu : MonoBehaviour
 {
     public float velocidad = 2f;
-
     private Rigidbody2D rb2d;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         rb2d.velocity = Vector2.right * velocidad;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }
--- Assets/Scripts/MenuPrincipal/GreedyGenerator.cs
+++ Assets/Scripts/MenuPrincipal/GreedyGenerator.cs
@@ -6,16 +6,9 @@ public class GreedyGenerator : MonoBehaviour
 {
     public GameObject greedyMenuPrefab;
     public float generatorTimer = 2f;
-    // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("CreateSomething", 0f, generatorTimer);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     void CreateSomething()
